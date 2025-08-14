ccf9277966023a261bea8e30d2261d6d1b553239
dannbeckersenac <daniel.bortoluzzi@prof.sc.senac.br>
2025-08-13 22:01:30 -0300

    Configuração do ambiente de embedding e upload com s3

---
+++ app/ia/utils.py
@@ -0,0 +1,36 @@
+import boto3
+from botocore.exceptions import ClientError
+from config import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_S3_REGION, AWS_S3_BUCKET
+from pypdf import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+s3 = boto3.client(
+    "s3",
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+    region_name=AWS_S3_REGION
+)
+
+def check_s3(s3):
+    try:
+        s3.head_bucket(Bucket=AWS_S3_BUCKET)
+        print("S3 está online e funcional")
+    except ClientError as e:
+        print("Erro ao conectar ao S3:", e)
+
+def upload_file(file_path, key):
+    s3.upload_file(file_path, AWS_S3_BUCKET, key)
+    return f"https://{AWS_S3_BUCKET}.s3.{AWS_S3_REGION}.amazonaws.com/{key}"
+def read_pdf(file_path):
+    reader = PdfReader(file_path)
+    text = "".join(page.extract_text() + "\n" for page in reader.pages)
+    return text
+def chunk_split(text, chunk_size=500, chunk_overlap=50):
+    
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=chunk_size,
+        chunk_overlap=chunk_overlap
+    )
+    
+    return splitter.split_text(text)
+
---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+AWS_S3_BUCKET = os.getenv("AWS_S3_BUCKET")
+AWS_S3_REGION = os.getenv("AWS_S3_REGION")
+
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+DATABASE_URL = os.getenv("DATABASE_URL")
